e7723dd0ff6050e54228414e6b7682814a83575b
Fotis Andritsos <fotisandritsos85@gmail.com>
2020-12-09 18:39:24 +0200

    functionality

--- Clients List - UI/script.js
+++ Clients List - UI/script.js
@@ -1,14 +1,17 @@
 const searchIcon = document.querySelector(".search-icon");
 const CloseIcon = document.querySelector(".close-icon");
 const search = document.querySelector(".search");
-const clientItem = document.querySelectorAll(".client-item");
+const clientList = document.querySelector(".client-list");
 const options = document.querySelectorAll(".options");
 const addItem = document.querySelector(".add-item");
 const modalClient = document.querySelector(".modal-client");
 const plusIcon = document.querySelector(".plus-icon");
 const closeItem = document.querySelector(".close-icon-item");
+const menuIcon = document.querySelector(".menu-icon");
+const menuItem = document.querySelector(".menu-item");
+const closeMenuItem = document.querySelector(".close-menu-item");
 
-
+// ADD CLIENT 
 addItem.addEventListener("click", () => {
   modalClient.classList.add("active");
   plusIcon.classList.add("active");
@@ -20,29 +23,35 @@ closeItem.addEventListener("click", () => {
   plusIcon.classList.remove("active");
 });
 
-
+// SEARCH 
 searchIcon.addEventListener("click", () => {
   search.classList.add("active");
 });
+
 CloseIcon.addEventListener("click", () => {
   search.classList.remove("active");
 });
 
-const listOptions = (event) => {
-  const tar = Array.from(clientItem).indexOf(event.target);
-  tri = event.target;
-  const yo = options[tar];
-  const ro = clientItem[tar];
-  yo.classList.toggle("active");
-  ro.classList.toggle("active");
-};
 
-clientItem.forEach((item) => {
-  item.addEventListener("click", listOptions);
+
+let client = clientList.childNodes;
+
+client.forEach((cli,ind) => {
+  let gii = cli.childNodes;
+  for(let x=0; x<gii.length; x++){
+    gii[x].addEventListener('click',e =>{
+      let lol = e.target;
+      client[ind].classList.toggle("active");
+    })
+  }
+})
+
+// menu
+menuIcon.addEventListener("click", () => {
+  menuItem.classList.add("active");
+
 });
 
-
-function newFunction() {
-  console.log("ffdsdfsdfsd");
-}
-
+closeMenuItem.addEventListener("click", () => {
+  menuItem.classList.remove("active");
+});
